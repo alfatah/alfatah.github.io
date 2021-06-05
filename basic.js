89d778bbf416b4d736f1631733d2010ec8fdb0fa
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2021-06-06 02:57:30 +0700

    Update basic.js

--- basic.js
+++ basic.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
     function getLocationF() {
         $.getJSON("https://ipapi.co/json/", function(ip) {
             console.log(ip);
-            $("#ip-address").html("IP : " + ip.ip + " " + ip.org + " " + ip.asn);
+            $("#ip-address").html("Your IP : " + ip.ip + "," + " " + ip.org + " " + ip.asn);
             $("#location-data").html(ip.latitude + "," + ip.longitude + " " + ip.city + ", " + ip.region + " " + ip.postal + " " + ip.country_name);
+            $("#timezone").html("Population Country : " + ip.country_population + " ");
 
             getWeatherF(ip.postal);
         }, 'jsonp');
