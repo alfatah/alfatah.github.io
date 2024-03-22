b92be0dde58d4676889dd87aec9962af5be15328
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-23 04:05:47 +0700

    update

--- basic.js
+++ basic.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
             $("#location-data").html(ip.latitude + "," + ip.longitude + " " + ip.city + ", " + ip.region + " " + ip.postal + " " + ip.country_name);
             $("#population").html("Population Country : " + ip.country_population + " ");
 
-            // Mendapatkan kode negara dari data IP
+            // Get country code from IP data
             var countryCode = ip.country;
             getGDP(countryCode);
             
             getWeatherF(ip.postal);
-        }, 'jsonp');
+        });
     }
     
-    // Fungsi untuk mendapatkan data PDB negara menggunakan kode negara
+    // Function to get GDP data of a country using country code
     function getGDP(countryCode) {
-        $.getJSON(`http://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GDP.MKTP.CD?format=json`, function(data) {
+        $.getJSON(`https://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GDP.MKTP.CD?format=json`, function(data) {
             var gdp = "Not Available";
             if (data[1] && data[1][0] && data[1][0].value) {
                 gdp = data[1][0].value;
